50f7a3c6352221582eccb092e19a824f264c8a44
AndyRachmat27 <80530325+AndyRachmat27@users.noreply.github.com>
2024-06-05 19:16:17 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'June 08, 2024',
+  birthdate: 'Feb 02, 2022',
   name: 'MITA SAYANGG ❤️'
 };
 
